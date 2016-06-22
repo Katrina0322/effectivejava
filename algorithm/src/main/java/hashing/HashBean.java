da5e48e4d80eb406e309a69b7466ecb6dbf2e170
ubuntu <eternity_bliss@sina.cn>
2016-06-22 18:21:35 +0800

    2016.06.22

--- algorithm/src/main/java/hashing/HashBean.java
+++ algorithm/src/main/java/hashing/HashBean.java
@@ -5,4 +5,30 @@ package hashing;
  * Created by tianjin on 6/22/16.
  */
 public class HashBean {
+
+    private String name;
+
+    private double a;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        HashBean hashBean = (HashBean) o;
+
+        if (Double.compare(hashBean.a, a) != 0) return false;
+        return name != null ? name.equals(hashBean.name) : hashBean.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        temp = Double.doubleToLongBits(a);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
--- algorithm/src/main/java/hashing/SeparateChainingHashTable.java
+++ algorithm/src/main/java/hashing/SeparateChainingHashTable.java
@@ -1,8 +1,26 @@
 package hashing;
 
+import java.util.List;
+
 /**
  * used to
  * Created by tianjin on 6/22/16.
  */
-public class SeparateChainingHashTable {
+public class SeparateChainingHashTable<T> {
+    private static final int DEFAULT_TABLE_SIZE = 101;
+
+    private int currentSize;
+
+    private List<T>[] theLists;
+
+    private int myHash(T t){
+        int hashVal = t.hashCode();
+
+        hashVal %= theLists.length;
+
+        if(hashVal < 0){
+            hashVal =+ theLists.length;
+        }
+        return hashVal;
+    }
 }
