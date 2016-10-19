08b1ad5dac8e91db361da503d6faad14c302d0b9
ubuntu <eternity_bliss@sina.cn>
2016-10-19 19:13:55 +0800

    由spark2.0.1改到1.5.2

--- sparkanalysis/src/main/java/com/oneapm/performance/SSDPerformanceTest.java
+++ sparkanalysis/src/main/java/com/oneapm/performance/SSDPerformanceTest.java
@@ -1,34 +1,23 @@
 package com.oneapm.performance;
 
+import base.Analysis;
 import com.google.common.collect.ImmutableMap;
 import com.oneapm.javabean.DBData;
 import com.oneapm.javabean.DBKey;
-import kafka.serializer.DefaultDecoder;
-import kafka.serializer.StringDecoder;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.logging.LogFactory;
-import org.apache.spark.SparkConf;
+import org.apache.commons.net.util.Base64;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
-import org.apache.spark.api.java.function.VoidFunction;
-import org.apache.spark.storage.StorageLevel;
-import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaPairReceiverInputDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.apache.spark.streaming.kafka.KafkaUtils;
 import org.elasticsearch.hadoop.cfg.ConfigurationOptions;
 import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Tuple2;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Pattern;
@@ -37,12 +26,10 @@ import java.util.regex.Pattern;
  * used to
  * Created by tianjin on 10/18/16.
  */
-public class SSDPerformanceTest {
+public class SSDPerformanceTest extends Analysis{
     public static final Logger LOG = LoggerFactory.getLogger(SSDPerformanceTest.class);
 
     protected static final Pattern TAB = Pattern.compile("\t");
-
-    private static JavaStreamingContext jsc;
 
     private static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.CHINA);
 
@@ -61,9 +48,9 @@ public class SSDPerformanceTest {
         LOG.info("database config:" + config.toString());
 
 
-        rawStream.foreachRDD(new VoidFunction<JavaPairRDD<String, byte[]>>() {
+        rawStream.foreachRDD(new Function<JavaPairRDD<String, byte[]>, Void>() {
             @Override
-            public void call(JavaPairRDD<String, byte[]> stringJavaPairRDD) throws Exception {
+            public Void call(JavaPairRDD<String, byte[]> stringJavaPairRDD) throws Exception {
                 JavaRDD<Map<String, ?>> es = stringJavaPairRDD.mapToPair(new PairFunction<Tuple2<String, byte[]>, DBKey, DBData>() {
                     public Tuple2<DBKey, DBData> call(Tuple2<String, byte[]> stringTuple2) throws Exception {
                         String[] database = TAB.split(new String(stringTuple2._2));
@@ -72,7 +59,7 @@ public class SSDPerformanceTest {
                         DBData dbData = new DBData();
                         String sqlString = new String(Base64.decodeBase64(database[10].trim()));
                         String storageSql;
-                        if(sqlString.length() > 500){
+                        if(sqlString.length() > 1000){
                             storageSql = sqlString.substring(0,1000);
                         }else{
                             storageSql = sqlString;
@@ -175,9 +162,9 @@ public class SSDPerformanceTest {
                     JavaEsSpark.saveToEs(es, "ni-database-{index_name}/database", ImmutableMap.of
                             (ConfigurationOptions.ES_MAPPING_EXCLUDE, "index_name"));
                 }
+                return null;
             }
         });
-
 
         rawStream.context().start();
         rawStream.context().awaitTermination();
@@ -185,48 +172,4 @@ public class SSDPerformanceTest {
     }
 
 
-    protected static JavaPairReceiverInputDStream<String, byte[]> setupRawStreamFromKafka(Properties config, String appId) {
-        String master = config.getProperty("spark.master");
-        String es_node = config.getProperty("es.node");
-        String checkpoint_dir = config.getProperty("spark.checkpoint.dir");
-        int TIME_WINDOW_SEC = Integer.parseInt(config.getProperty("time.window"));
-        String[] topic_list = config.getProperty("topic.list").split(",");
-        String zoo = config.getProperty("kafaka.zoo");
-        String maxCores = config.getProperty("spark.cores", "2");
-        String memory = config.getProperty("spark.memory", "1g");
-        HashMap topicSetting = new HashMap();
-        String[] kafkaSetting = topic_list;
-        int sparkConf = topic_list.length;
-
-        for (int self = 0; self < sparkConf; ++self) {
-            String topic = kafkaSetting[self];
-            topicSetting.put(topic, Integer.valueOf(1));
-        }
-
-        HashMap var15 = new HashMap();
-        var15.put("zookeeper.connect", zoo);
-        var15.put("group.id", appId);
-        var15.put("zookeeper.connection.timeout.ms", "10000");
-        SparkConf var14 = (new SparkConf()).setAppName(appId).set("spark.executor.memory", memory).set("spark.cores.max", String.valueOf(maxCores)).set("es.nodes", es_node).set("spark.executor.extraJavaOptions", "-Djava.net.preferIPv4Stack=true -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly").setMaster(master);
-        String var16 = getSelfFilePath();
-        jsc = new JavaStreamingContext(var14, Durations.seconds((long)TIME_WINDOW_SEC));
-        if(checkpoint_dir != null) {
-            jsc.checkpoint(checkpoint_dir);
-        }
-
-        return KafkaUtils.createStream(jsc, String.class, byte[].class, StringDecoder.class, DefaultDecoder.class, var15, topicSetting, StorageLevel.MEMORY_AND_DISK_SER());
-    }
-
-    private static String getSelfFilePath() {
-        String path = SSDPerformanceTest.class.getProtectionDomain().getCodeSource().getLocation().getFile();
-
-        try {
-            path = URLDecoder.decode(path, "UTF-8");
-        } catch (UnsupportedEncodingException var2) {
-            LogFactory.getLog(SSDPerformanceTest.class).error(var2);
-        }
-
-        File jarFile = new File(path);
-        return jarFile.isDirectory()?null:jarFile.getAbsolutePath();
-    }
 }
--- sparkanalysis/src/main/java/com/oneapm/producer/KafkaDataProducer.java
+++ sparkanalysis/src/main/java/com/oneapm/producer/KafkaDataProducer.java
@@ -23,24 +23,27 @@ public class KafkaDataProducer {
 
     public KafkaDataProducer() {
         Properties props = new Properties();
-        props.put("metadata.broker.list", Config.getConfig("database.cnf").getProperty("kafaka.zoo"));
-        props.put("serializer.class", "kafka.serializer.StringEncoder");
-        props.put("key.serializer.class", "kafka.serializer.StringEncoder");
-        props.put("request.required.acks", "-1");
+        props.put("bootstrap.servers", Config.getConfig("database.cnf").getProperty("bootstrap.server"));
+//        props.put("metadata.broker.list", Config.getConfig("database.cnf").getProperty("kafaka.zoo"));
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         producer = new KafkaProducer<>(props);
     }
 
     private void produce() throws Exception {
         BufferedReader br = null;
         try {
-            br = new BufferedReader(new FileReader(Config.getConfig("database.cnf").getProperty("datafile")));
-            String line;
+            while ( true ) {
+                br = new BufferedReader(new FileReader(Config.getConfig("database.cnf").getProperty("datafile")));
+                String line;
 
 
-            while ((line = br.readLine()) != null) {
-                if (!"".equals(line.trim())) {
-                    producer.send(new ProducerRecord<>("test", "", line));
+                while ((line = br.readLine()) != null) {
+                    if (!"".equals(line.trim())) {
+                        producer.send(new ProducerRecord<>("test", "", line));
+                    }
                 }
+                Thread.sleep(Long.valueOf(Config.getConfig("database.cnf").getProperty("sleep.time")));
             }
         } catch (Exception e) {
             log.error("The read streaming error: ", e);
