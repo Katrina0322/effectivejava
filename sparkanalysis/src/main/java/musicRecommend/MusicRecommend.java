de53ce11be482ed46a507e0557f4b1dc615896d3
ubuntu <eternity_bliss@sina.cn>
2016-11-14 18:41:55 +0800

    ALS推荐

--- sparkanalysis/src/main/java/musicRecommend/MusicRecommend.java
+++ sparkanalysis/src/main/java/musicRecommend/MusicRecommend.java
@@ -7,7 +7,13 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.broadcast.Broadcast;
+import org.apache.spark.mllib.recommendation.ALS;
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel;
+import org.apache.spark.mllib.recommendation.Rating;
 import scala.Tuple2;
+
+import java.util.List;
 
 /**
  * music recommend
@@ -18,15 +24,9 @@ public class MusicRecommend {
         SparkConf sparkConf = new SparkConf();
         JavaSparkContext jsc = new JavaSparkContext("local[2]", "music", sparkConf);
 
-        JavaRDD<Double> user_artist = jsc.textFile("hdfs://10.128.9.220:9000/tianjin/musicdata/user_artist_data.txt").map(new Function<String, Double>() {
-            @Override
-            public Double call(String v1) throws Exception {
 
-                return Double.parseDouble(v1.split(" ")[0]);
-            }
-        });
 
-        JavaPairRDD<Integer,String> artist = jsc.textFile("hdfs://10.128.9.220:9000/tianjin/musicdata/artist_data.txt").mapToPair(new PairFunction<String, Integer, String>() {
+        JavaPairRDD<Integer,String> artistData = jsc.textFile("hdfs://10.128.9.220:9000/tianjin/musicdata/artist_data.txt").mapToPair(new PairFunction<String, Integer, String>() {
             @Override
             public Tuple2<Integer, String> call(String s) throws Exception {
                 Tuple2<Integer,String> tuple = null;
@@ -47,6 +47,7 @@ public class MusicRecommend {
                 return v1 != null;
             }
         });
+
 
         JavaPairRDD<Integer,Integer> artistAlias = jsc.textFile("hdfs://10.128.9.220:9000/tianjin/musicdata/user_artist_data.txt").mapToPair(new PairFunction<String, Integer, Integer>() {
             @Override
@@ -69,6 +70,26 @@ public class MusicRecommend {
 
 
 
+
+        final Broadcast<JavaPairRDD<Integer,Integer>> artistAliasBroad = jsc.broadcast(artistAlias);
+
+        JavaRDD<Rating> user_artist = jsc.textFile("hdfs://10.128.9.220:9000/tianjin/musicdata/user_artist_data.txt").map(new Function<String, Rating>() {
+            @Override
+            public Rating call(String v1) throws Exception {
+                String[] s = v1.split(" ");
+                int userID = Integer.parseInt(s[0]);
+                int artistID =Integer.parseInt(s[1]);
+                double count = Double.parseDouble(s[2]);
+                List<Integer> alias = artistAliasBroad.value().lookup(artistID);
+                if(!alias.isEmpty()){
+                   userID = alias.get(0);
+                }
+                return new Rating(userID,artistID,count);
+            }
+        }).cache();
+
+
+        MatrixFactorizationModel model = ALS.trainImplicit(JavaRDD.toRDD(user_artist),10,5,0.01,1.0);
 
 
 
