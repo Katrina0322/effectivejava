c69d582bfe7076b33f0120a36e7473379a0a706d
spark <engimatic_child@sina.com>
2016-12-01 00:00:29 +0800

    字符串查找

---
+++ algorithm/src/main/java/stringsort/MSD.java
@@ -0,0 +1,24 @@
+package stringsort;
+
+/**
+ * Created by spark on 11/30/16.
+ */
+public class MSD {
+    private static int R = 256;
+    private static final int M = 15;
+    private static String[] aux;
+
+    private static int charAt(String s,int d){
+        return d < s.length() ? s.charAt(d) : -1;
+    }
+
+    public static void sort(String[] a){
+        int N = a.length;
+        aux = new String[N];
+
+    }
+
+    private static void sort(String[] a,int lo,int hi,int d){
+        
+    }
+}
