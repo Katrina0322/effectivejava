716eeed5acc1431cfddefc736cab6bcbcce5dd8f
spark <engimatic_child@sina.com>
2016-12-05 23:20:21 +0800

    Elastic Net

--- sparkanalysis/src/main/java/linearRegression/LinearRegressionExample.java
+++ sparkanalysis/src/main/java/linearRegression/LinearRegressionExample.java
@@ -17,7 +17,9 @@ import org.apache.spark.sql.SQLContext;
  * Created by tianjin on 12/5/16.
  */
 public class LinearRegressionExample {
-
+    /**
+     * 日志控制
+     */
     static{
         LogSetting.setWarningLogLevel("org");
         LogSetting.setWarningLogLevel("akka");
@@ -30,19 +32,23 @@ public class LinearRegressionExample {
         String resources = Thread.currentThread().getContextClassLoader().getResource("").getPath();
 //        PropertyConfigurator.configure(resources + "log4j.properties");
         System.out.println(resources);
-        SparkConf conf = new SparkConf().setAppName("Linear Regression Example").setMaster("local[2]");
+        SparkConf conf = new SparkConf().setAppName("Logistic Regression with Elastic Net Example").setMaster("local[2]");
         SparkContext sc = new SparkContext(conf);
         SQLContext sql = new SQLContext(sc);
 
         String path  = resources + "libsvm_data.txt";
         DataFrame training = sql.createDataFrame(MLUtils.loadLibSVMFile(sc, path).toJavaRDD(), LabeledPoint.class);
+
         LinearRegression lr = new LinearRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8);
         LinearRegressionModel lrModel = lr.fit(training);
         System.out.println("Weights: " + lrModel.weights() + " Intercept: " + lrModel.intercept());
+            
         LinearRegressionTrainingSummary trainingSummary = lrModel.summary();
         System.out.println("numIterations: " + trainingSummary.totalIterations());
         System.out.println("objectiveHistory: " + Vectors.dense(trainingSummary.objectiveHistory()));
+
         trainingSummary.residuals().show();
+
         System.out.println("RMSE: " + trainingSummary.rootMeanSquaredError());
         System.out.println("r2: " + trainingSummary.r2());
     }
