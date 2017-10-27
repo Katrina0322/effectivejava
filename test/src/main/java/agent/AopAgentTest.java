ef050904f0a2a8900ea0eabf046edcb3273b9d0d
ubuntu <eternity_bliss@sina.cn>
2017-10-27 18:28:35 +0800

    test

---
+++ test/src/main/java/agent/AopAgentTest.java
@@ -0,0 +1,15 @@
+package agent;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * filename: AopAgentTest
+ * Description:
+ * Author: ubuntu
+ * Date: 10/26/17 10:16 AM
+ */
+public class AopAgentTest {
+    public static void premain(String agentArgs, Instrumentation inst){
+
+    }
+}
