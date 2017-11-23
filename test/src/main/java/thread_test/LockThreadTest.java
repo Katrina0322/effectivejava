0f96efd002a73db3022850451e672e913a4c9c9f
ubuntu <eternity_bliss@sina.cn>
2017-11-23 18:35:11 +0800

    die lock

--- test/src/main/java/thread_test/LockThreadTest.java
+++ test/src/main/java/thread_test/LockThreadTest.java
@@ -13,22 +13,28 @@ import java.util.concurrent.locks.ReentrantLock;
  * Date: 11/22/17 5:39 PM
  */
 public class LockThreadTest {
-    private static Lock lock = new ReentrantLock();
+//    private static Lock lock = new ReentrantLock();
     private static ExecutorService service = SharedPool.EXECUTOR;
 
     public static void test(){
-//        for(int i = 0; i < 100; i++){
-//            service.submit(new Runnable() {
-//                @Override
-//                public void run() {
-//                    lock.lock();
-//                    while (true) {
-//                        System.out.println(Thread.currentThread().getName());
-//                    }
-//                }
-//            });
-//        }
-//        lock.unlock();
+        for(int i = 0; i < 100; i++){
+            final int finalI = i;
+            service.submit(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        System.out.println(Thread.currentThread().getName() + ":" + finalI);
+
+                        try {
+                            wait(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            });
+        }
     }
 
     public static void main(String[] args) {
