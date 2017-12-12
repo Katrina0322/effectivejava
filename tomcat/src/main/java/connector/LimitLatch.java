672fbcd5101e1a6b7fa435678400d6c71b2a6e3a
ubuntu <eternity_bliss@sina.cn>
2017-12-12 18:37:22 +0800

    LimitLatch

---
+++ tomcat/src/main/java/connector/LimitLatch.java
@@ -0,0 +1,16 @@
+package connector;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * filename: LimitLatch
+ * Description:
+ * Author: ubuntu
+ * Date: 12/12/17 11:24 AM
+ */
+public class LimitLatch{
+
+    private static final class sync extends AbstractQueuedSynchronizer{
+
+    }
+}
---
+++ tomcat/src/main/java/connector/NioConnector.java
@@ -0,0 +1,13 @@
+package connector;
+
+/**
+ * filename: NioConnector
+ * Description:
+ * Author: ubuntu
+ * Date: 12/12/17 10:23 AM
+ */
+public class NioConnector {
+    public static void main(String[] args) {
+
+    }
+}
