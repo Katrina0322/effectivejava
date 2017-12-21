b6f3aeb6c5693549915fca96a06daa68677fe657
ubuntu <eternity_bliss@sina.cn>
2017-12-21 18:16:37 +0800

    jsonobject

--- flinkanalysis/src/main/java/kafka/ApmTrans.java
+++ flinkanalysis/src/main/java/kafka/ApmTrans.java
@@ -7,4 +7,161 @@ package kafka;
  * Date: 12/21/17 4:28 PM
  */
 public class ApmTrans {
+    private String Agent;
+    private String Tier;        //接收方集群
+    private String entranceService;     //入口事物
+    private String language;
+    private String webService;      //web事物
+    private long startTime;
+    private String componentName;
+
+    private double responseTime;    //总响应时间
+    private double maxRespTime;     //每颗粒度最大响应时间
+    private double minRespTime;     //每颗粒度最小响应时间
+    private int trans_count;        //调用次数
+    private int is_errorResponse;   //错误次数
+    private int is_fastResponse;    //满意次数
+    private int is_tolerableResponse;   //容忍次数
+    private int is_slowResponse;    //缓慢次数
+
+    public String getAgent() {
+        return Agent;
+    }
+
+    public void setAgent(String agent) {
+        Agent = agent;
+    }
+
+    public String getTier() {
+        return Tier;
+    }
+
+    public void setTier(String tier) {
+        Tier = tier;
+    }
+
+    public String getEntranceService() {
+        return entranceService;
+    }
+
+    public void setEntranceService(String entranceService) {
+        this.entranceService = entranceService;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getWebService() {
+        return webService;
+    }
+
+    public void setWebService(String webService) {
+        this.webService = webService;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getComponentName() {
+        return componentName;
+    }
+
+    public void setComponentName(String componentName) {
+        this.componentName = componentName;
+    }
+
+    public double getResponseTime() {
+        return responseTime;
+    }
+
+    public void setResponseTime(double responseTime) {
+        this.responseTime = responseTime;
+    }
+
+    public double getMaxRespTime() {
+        return maxRespTime;
+    }
+
+    public void setMaxRespTime(double maxRespTime) {
+        this.maxRespTime = maxRespTime;
+    }
+
+    public double getMinRespTime() {
+        return minRespTime;
+    }
+
+    public void setMinRespTime(double minRespTime) {
+        this.minRespTime = minRespTime;
+    }
+
+    public int getTrans_count() {
+        return trans_count;
+    }
+
+    public void setTrans_count(int trans_count) {
+        this.trans_count = trans_count;
+    }
+
+    public int getIs_errorResponse() {
+        return is_errorResponse;
+    }
+
+    public void setIs_errorResponse(int is_errorResponse) {
+        this.is_errorResponse = is_errorResponse;
+    }
+
+    public int getIs_fastResponse() {
+        return is_fastResponse;
+    }
+
+    public void setIs_fastResponse(int is_fastResponse) {
+        this.is_fastResponse = is_fastResponse;
+    }
+
+    public int getIs_tolerableResponse() {
+        return is_tolerableResponse;
+    }
+
+    public void setIs_tolerableResponse(int is_tolerableResponse) {
+        this.is_tolerableResponse = is_tolerableResponse;
+    }
+
+    public int getIs_slowResponse() {
+        return is_slowResponse;
+    }
+
+    public void setIs_slowResponse(int is_slowResponse) {
+        this.is_slowResponse = is_slowResponse;
+    }
+
+    @Override
+    public String toString() {
+        return "ApmTrans{" +
+                "Agent='" + Agent + '\'' +
+                ", Tier='" + Tier + '\'' +
+                ", entranceService='" + entranceService + '\'' +
+                ", language='" + language + '\'' +
+                ", webService='" + webService + '\'' +
+                ", startTime=" + startTime +
+                ", componentName='" + componentName + '\'' +
+                ", responseTime=" + responseTime +
+                ", maxRespTime=" + maxRespTime +
+                ", minRespTime=" + minRespTime +
+                ", trans_count=" + trans_count +
+                ", is_errorResponse=" + is_errorResponse +
+                ", is_fastResponse=" + is_fastResponse +
+                ", is_tolerableResponse=" + is_tolerableResponse +
+                ", is_slowResponse=" + is_slowResponse +
+                '}';
+    }
 }
--- flinkanalysis/src/main/java/kafka/FlinkWithKafkaComsumer.java
+++ flinkanalysis/src/main/java/kafka/FlinkWithKafkaComsumer.java
@@ -1,12 +1,27 @@
 package kafka;
 
 
+import com.alibaba.fastjson.JSONObject;
+import org.apache.flink.api.common.ExecutionConfig;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeutils.TypeSerializer;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.streaming.api.windowing.assigners.WindowAssigner;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.triggers.Trigger;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.api.windowing.windows.Window;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
 
+import java.util.Collection;
 import java.util.Properties;
 
 /**
@@ -21,9 +36,9 @@ public class FlinkWithKafkaComsumer {
 
     public static void main(String[] args) throws Exception {
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "10.128.9.207:9092");
+        properties.setProperty("bootstrap.servers", "10.128.5.6:9092");
         properties.setProperty("group.id", "test");
-        FlinkKafkaConsumer09<String> kafkaConsumer09 = new FlinkKafkaConsumer09<>("ni-tcp", new SimpleStringSchema(), properties);
+        FlinkKafkaConsumer09<String> kafkaConsumer09 = new FlinkKafkaConsumer09<>("apm-web-trans-agg-1", new SimpleStringSchema(), properties);
         kafkaConsumer09.setStartFromEarliest();
         DataStream<String> stream = env.addSource(kafkaConsumer09);
 //       stream.addSink(new SinkFunction<String>() {
@@ -32,7 +47,51 @@ public class FlinkWithKafkaComsumer {
 //               System.out.println(value);
 //           }
 //       });
-        stream.print();
+        DataStream<ApmTrans> apm = stream.map(new MapFunction<String, ApmTrans>() {
+            private static final long serialVersionUID = 5410801009568806675L;
+
+            @Override
+            public ApmTrans map(String s) throws Exception {
+                return JSONObject.parseObject(s, ApmTrans.class);
+            }
+        });
+
+        KeyedStream<ApmTrans, Long> key = apm.keyBy(new KeySelector<ApmTrans, Long>() {
+            private static final long serialVersionUID = 1495012476835593818L;
+
+            @Override
+            public Long getKey(ApmTrans apmTrans) throws Exception {
+                return apmTrans.getStartTime() / 1000;
+            }
+        });
+
+        WindowedStream<ApmTrans, Long, TimeWindow> windows = key.timeWindow(Time.seconds(30));
+        SingleOutputStreamOperator<ApmTrans> sum = windows.sum("is_tolerableResponse");
+
+//        key.window(new WindowAssigner<ApmTrans, Window>() {
+//            private static final long serialVersionUID = -8854510666154522903L;
+//
+//            @Override
+//            public Collection<Window> assignWindows(ApmTrans apmTrans, long l, WindowAssignerContext windowAssignerContext) {
+//                return null;
+//            }
+//
+//            @Override
+//            public Trigger<ApmTrans, Window> getDefaultTrigger(StreamExecutionEnvironment streamExecutionEnvironment) {
+//                return null;
+//            }
+//
+//            @Override
+//            public TypeSerializer<Window> getWindowSerializer(ExecutionConfig executionConfig) {
+//                return null;
+//            }
+//
+//            @Override
+//            public boolean isEventTime() {
+//                return false;
+//            }
+//        });
+        sum.print();
         env.execute();
     }
 }
