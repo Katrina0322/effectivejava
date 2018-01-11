5e553beaba68123f6d483a9624cac4d2b317fae0
ubuntu <eternity_bliss@sina.cn>
2018-01-11 19:11:44 +0800

    memstore写入

---
+++ database/src/main/java/com/effective/hermes/cache/Column.java
@@ -0,0 +1,10 @@
+package com.effective.hermes.cache;
+
+/**
+ * filename: Column
+ * Description:
+ * Author: ubuntu
+ * Date: 1/11/18 2:23 PM
+ */
+public class Column {
+}
---
+++ database/src/main/java/com/effective/hermes/cache/ColumnFamily.java
@@ -0,0 +1,10 @@
+package com.effective.hermes.cache;
+
+/**
+ * filename: ColumnFamily
+ * Description:
+ * Author: ubuntu
+ * Date: 1/11/18 2:15 PM
+ */
+public class ColumnFamily {
+}
