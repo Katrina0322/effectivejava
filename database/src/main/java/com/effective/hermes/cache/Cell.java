7f60656fe32b8184305e08dbf3a443eb67fbd7b3
ubuntu <eternity_bliss@sina.cn>
2018-01-19 18:03:03 +0800

    cell单元简化

--- database/src/main/java/com/effective/hermes/cache/Cell.java
+++ database/src/main/java/com/effective/hermes/cache/Cell.java
@@ -2,29 +2,16 @@ package com.effective.hermes.cache;
 
 /**
  * filename: Cell
- * Description: 大量的数据冗余,每一个cell都重复存储了row,family,timestaml
+ * Description:
  * Author: ubuntu
  * Date: 1/8/18 10:07 AM
  */
 public interface Cell {
     byte[] getRowArray();
-    int getRowOffset();
-    short getRowLength();
-
-    byte[] getFamilyArray();
-    int getFamilyOffset();
-    byte getFamilyLength();
-
-    byte[] getQualifierArray();
-    int getQualifierLength();
-    int getQualifierOffset();
 
     long getTimestamp();
 
     byte[] getValueArray();
-    int getValueOffset();
-    int getValueLength();
-
 
     Type getType();
     enum Type {
@@ -34,9 +21,7 @@ public interface Cell {
 
         DeleteFamilyVersion((byte) 10),
 
-        DeleteColumn((byte) 12),
-
-        DeleteFamily((byte) 14);
+        DeleteColumn((byte) 12);
 
         private final byte code;
 
