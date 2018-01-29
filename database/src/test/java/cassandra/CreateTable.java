97392c7ecc02d58b2d29d6c045d9d3fa8f16b32a
engimatic <eternity_bliss@sina.cn>
2018-01-29 23:32:33 +0800

    create test

--- database/src/test/java/cassandra/CreateTable.java
+++ database/src/test/java/cassandra/CreateTable.java
@@ -15,13 +15,21 @@ public class CreateTable {
     private Session session;
     @Before
     public void init(){
-        Cluster.Builder builder1 = Cluster.builder().addContactPoint("10.128.9.207");
+        Cluster.Builder builder1 = Cluster.builder().addContactPoint("127.0.0.1");
         Cluster cluster = builder1.build();
-        this.session = cluster.connect( );
+        this.session = cluster.connect();
+//        String query = "CREATE KEYSPACE tp WITH replication "
+//                + "= {'class':'SimpleStrategy', 'replication_factor':1}; ";
+//        session.execute(query);
     }
 
     @Test
     public void createTable(){
-
+        String query = "CREATE TABLE tp.emp(emp_id int PRIMARY KEY, "
+                + "emp_name text, "
+                + "emp_city text, "
+                + "emp_sal varint, "
+                + "emp_phone varint );";
+        session.execute(query);
     }
 }
