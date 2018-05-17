24c5e94b8b50f8ae7f3832b1fe76b624ec827aae
engimatic <eternity_bliss@sina.cn>
2018-05-17 19:31:01 +0800

    type

--- recommend/src/main/java/com/ronglian/core/RecModelType.java
+++ recommend/src/main/java/com/ronglian/core/RecModelType.java
@@ -4,5 +4,23 @@ package com.ronglian.core;
  * Created by spark on 18-5-3.
  */
 public enum RecModelType {
+    HOT_ISSUE(0, "hotIssue"),
+    USER_ITEM_CF(1, "userItemCF"),
+    CONTENT_BASE(2, "contentBase"),
+    USER_FEATURE(3, "userFeature");
+    private int index;
+    private String name;
 
+    RecModelType(int index, String name) {
+        this.index = index;
+        this.name = name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
