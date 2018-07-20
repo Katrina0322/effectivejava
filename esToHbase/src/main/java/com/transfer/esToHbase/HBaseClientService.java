d356c05dce31f12c613dadfca61df7c8e322e765
engimatic <eternity_bliss@sina.cn>
2018-07-20 18:05:48 +0800

    a

---
+++ esToHbase/src/main/java/com/transfer/esToHbase/HBaseClientService.java
@@ -0,0 +1,11 @@
+package com.transfer.esToHbase;
+
+/**
+ * @description:
+ * @author: tianjin
+ * @email: eternity_bliss@sina.cn
+ * @create: 2018-07-19 下午5:03
+ */
+
+public class HBaseClientService {
+}
